548e6976fa61bdc5fb4d8e0d01e24016146ceec1
jyootai <jyootg@gmail.com>
2014-12-27 12:19:54 +0800

    init users routing rspec

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type=>:routing do
+  describe "users routing" do
+    it "routings to #show" do
+      expect(get("/admin")).to route_to(:controller=>"users",
+                                       :action=>"show",
+                                       :username=>"admin")
+    end
+  end
 end
