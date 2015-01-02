534cab7bf8d55cde63c4c58d02941606eaf01c44
jyootai <jyootg@gmail.com>
2015-01-02 22:18:46 +0800

    finish users/sessions routes rspec

--- spec/routing/users/sessions_routing_spec.rb
+++ spec/routing/users/sessions_routing_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Users::SessionsController, :type=>:routing do
+  it "routes to #new" do
+    expect(:get=>"/user/login").to route_to(:controller=>"users/sessions",
+                                           :action=>"new")
+
+  end
+
+  it "routes to #destroy" do
+    expect(:get=>"/user/logout").to route_to(:controller=>"users/sessions",
+                                           :action=>"destroy")
+
+  end
+  it "routes to #create" do
+    expect(:post=>"/user/login").to route_to(:controller=>"users/sessions",
+                                           :action=>"create")
+
+  end
 end
